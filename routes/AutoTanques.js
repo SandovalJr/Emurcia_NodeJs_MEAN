4a30769b29fb26d43fec821f8e4d28c3954fa8de
Eduardo Sandoval <edubizzle7@gmail.com>
2020-08-03 17:20:57 -0500

    se creo modelo de auto tanques

---
+++ routes/AutoTanques.js
@@ -0,0 +1,55 @@
+const Sequelize = require("sequelize"); //para usar el modelo del user
+const db = require("../database/db.js"); //datos de nuestra BD
+
+module.exports = db.sequelize.define(
+  "unidades_autoTanques",
+  {
+    id_autoTanque: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Num_eco: {
+      type: Sequelize.INTEGER,
+    },
+    anio: {
+      type: Sequelize.INTEGER,
+    },
+    ubicacion: {
+      type: Sequelize.STRING,
+    },
+    num_serie: {
+      type: Sequelize.STRING,
+    },
+    marca_auto: {
+      type: Sequelize.STRING,
+    },
+    modelo: {
+      type: Sequelize.STRING,
+    },
+    placas: {
+      type: Sequelize.STRING,
+    },
+    chofer_ruta: {
+      type: Sequelize.STRING,
+    },
+    cilindros_piezas: {
+      type: Sequelize.INTEGER,
+    },
+    observaciones: {
+      type: Sequelize.STRING,
+    },
+    AutoTanque: {
+      type: Sequelize.INTEGER,
+    },
+    Porcentaje_Salida_Llegada: {
+      type: Sequelize.INTEGER,
+    },
+    marca: {
+      type: Sequelize.STRING,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
