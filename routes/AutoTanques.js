0df8b5dfa8dadb8671c2cbf4ac5444cf50adab76
Eduardo Sandoval <edubizzle7@gmail.com>
2020-08-03 17:28:13 -0500

    api creada oara auto tanque

--- routes/AutoTanques.js
+++ routes/AutoTanques.js
@@ -1,59 +1,15 @@
-const Sequelize = require("sequelize"); //para usar el modelo del user
-const db = require("../database/db.js"); //datos de nuestra BD
+//aquí declaramos las rutas para nuestros Autos
+const express = require("express");
+const autos_tanques = express.Router();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const AutoTanque = require("../models/AuntoTanque");
+autos_tanques.use(cors());
 
-module.exports = db.sequelize.define(
-  "unidades_autoTanques",
-  {
-    id_autoTanque: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    Num_eco: {
-      type: Sequelize.INTEGER,
-    },
-    anio: {
-      type: Sequelize.INTEGER,
-    },
-    ubicacion: {
-      type: Sequelize.STRING,
-    },
-    num_serie: {
-      type: Sequelize.STRING,
-    },
-    marca_auto: {
-      type: Sequelize.STRING,
-    },
-    modelo: {
-      type: Sequelize.STRING,
-    },
-    placas: {
-      type: Sequelize.STRING,
-    },
-    chofer_ruta: {
-      type: Sequelize.STRING,
-    },
-    cilindros_piezas: {
-      type: Sequelize.INTEGER,
-    },
-    observaciones: {
-      type: Sequelize.STRING,
-    },
-    AutoTanque: {
-      type: Sequelize.INTEGER,
-    },
-    Porcentaje_Salida_Llegada: {
-      type: Sequelize.INTEGER,
-    },
-    marca: {
-      type: Sequelize.STRING,
-    },
-    created: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
+process.env.SECRET_KEY = "secret";
+
+autos_tanques.get("/", (req, res) => {
+  res.json({ status: "API AUTO TANQUE WORKS" });
+});
+
+module.exports = autos_tanques;
